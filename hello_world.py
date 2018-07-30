7a678520c3d9fac98c3879d26bde4dc5e99249ba
vafang25 <vivianfang25@gmail.com>
2018-07-30 10:09:47 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# Darby Plotsker
+# What does this program do? It says "hello"
+
+#condition = True
+
+if True:
+   print("Hello world!!!")
+else:
+   print("Whatevs")
+
+# answer = input("What is your favorite color?")
+# print("Your favorite color is  ", answer)
